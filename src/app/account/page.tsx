13a6786202cd80cb3c28a75c4e2bcd49af6f5fd1
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2024-05-02 23:59:37 -0300

    feat: Feed Conta (#0622)

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,13 +1,37 @@
+import photosGet from "@/actions/photos-get";
+import userGet from "@/actions/user-get";
+import Feed from "@/components/Feed";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Conta | Minha Conta",
 };
 
-export default function AccountPage() {
+export default async function AccountPage() {
+  const { data: user } = await userGet();
+  const { data } = await photosGet({ user: user?.username });
+
   return (
     <main>
-      <h1>Account</h1>
+      {data?.length ? (
+        <Feed photos={data} />
+      ) : (
+        <div>
+          <p
+            style={{ color: "#444", fontSize: "1.25rem", marginBottom: "1rem" }}
+          >
+            Nenhuma foto encontrada.
+          </p>
+          <Link
+            href={"account/post"}
+            className="button"
+            style={{ display: "inline-block" }}
+          >
+            Postar Fotos
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
