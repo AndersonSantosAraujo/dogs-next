75f4e13d1e793e6cf33f4a2e9d64a8ac843023cd
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2024-05-07 23:35:25 -0300

    feat: Photo Content (#0625)

--- src/app/photo/[id]/page.tsx
+++ src/app/photo/[id]/page.tsx
@@ -1,11 +1,29 @@
-export default async function PhotoIdPage({
-  params,
-}: {
-  params: { id: number };
-}) {
+import photoGet from "@/actions/photo-get";
+import PhotoContent from "@/components/Photo/PhotoContent";
+import { notFound } from "next/navigation";
+
+type PhotoIdParams = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: PhotoIdParams) {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return { title: "Photos" };
+  return {
+    title: data.photo.title,
+  };
+}
+
+export default async function PhotoIdPage({ params }: PhotoIdParams) {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return notFound();
   return (
-    <main>
-      <h1>Photo ID: {params.id}</h1>
-    </main>
+    <section className="container mainContainer">
+      <PhotoContent data={data} single={true} />
+    </section>
   );
 }
