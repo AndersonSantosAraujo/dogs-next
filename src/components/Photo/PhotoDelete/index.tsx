dbf3711c2d6f5c79a03e4aed2ca0210048c808ab
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2024-05-12 16:40:31 -0300

    feat: Photo Delete (#0626)

--- src/components/Photo/PhotoDelete/index.tsx
+++ src/components/Photo/PhotoDelete/index.tsx
@@ -1,3 +1,34 @@
-export default async function PhotoDeleteIndex({ id }: { id: string }) {
-  return <button>Deletar: {id}</button>;
+"use client";
+import { useState } from "react";
+import styles from "./styles.module.css";
+import photoDelete from "@/actions/photo-delete";
+
+export default function PhotoDeleteIndex({ id }: { id: string }) {
+  const [loading, setLoading] = useState(false);
+
+  async function handleClick() {
+    setLoading(true);
+
+    const confirm = window.confirm("Tem certezxa que deseja deletar?");
+
+    if (confirm) {
+      await photoDelete(id);
+    }
+
+    setLoading(false);
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deletar
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
 }
