539852363279d771ad5a6aaddac78701d9463fec
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2024-05-12 18:40:11 -0300

    feat: Lazy Loading (#0629)

--- src/app/account/statistics/page.tsx
+++ src/app/account/statistics/page.tsx
@@ -1,13 +1,25 @@
+import dynamic from "next/dynamic";
+import statsGet from "@/actions/stats-get";
+// import Statistics from "@/components/Account/Statistics";
 import { Metadata } from "next";
+
+// Importação diãmica para o LazyLoading
+const Statistics = dynamic(() => import("@/components/Account/Statistics"), {
+  loading: () => <p>Carregando...</p>,
+  ssr: false,
+});
 
 export const metadata: Metadata = {
   title: "Estatísticas | Minha Conta",
 };
 
 export default async function StatisticsPage() {
+  const { data } = await statsGet();
+
+  if (!data) return null;
   return (
-    <main>
-      <h1>Statistics</h1>
-    </main>
+    <section>
+      <Statistics data={data} />
+    </section>
   );
 }
