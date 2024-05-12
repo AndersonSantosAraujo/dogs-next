34c6ace3ccd03e5c0d0d033b760c423ad5fe08c5
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2024-05-12 19:04:54 -0300

    feat: Validate Token (#0630)

--- src/context/user-context.tsx
+++ src/context/user-context.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import logout from "@/actions/logout";
+import validateToken from "@/actions/validate-token";
 import {
   createContext,
   Dispatch,
   ReactNode,
   SetStateAction,
   useContext,
+  useEffect,
   useState,
 } from "react";
 
@@ -40,6 +43,15 @@ export function UserContextProvider({
   user: User | null;
 }) {
   const [userState, setUserState] = useState<User | null>(user);
+
+  useEffect(() => {
+    async function validate() {
+      const { ok } = await validateToken();
+      if (!ok) await logout();
+    }
+
+    if (userState) validate();
+  }, [userState]);
 
   return (
     <UserContext.Provider value={{ user: userState, setUserState }}>
