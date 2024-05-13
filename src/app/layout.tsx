7a0ec474de3a65d1248d85c5d997b85b175c0ae5
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2024-05-12 22:30:32 -0300

    feat: Deploy Vercel (#0701)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { UserContextProvider } from "@/context/user-context";
 import userGet from "@/actions/user-get";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Dogs Next",
@@ -30,6 +31,7 @@ export default async function RootLayout({
             <Footer />
           </div>
         </UserContextProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
